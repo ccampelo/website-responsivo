3713074134342b95af47ed9b0b138a83f5b8135a
Carlos Campelo <cecampelo1968@gmail.com>
2017-04-09 16:54:10 -0300

    Alteração de h1 para h2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 	$dt = date('d-m-Y');
-	echo "<br><h1>Olá Mundo programando com PHP!</h1><br>";
+	echo "<br><h2>Olá Mundo programando com PHP!</h2><br>";
 	echo phpinfo();
 	echo "<br><br>Data da consulta: ".$dt;
 
